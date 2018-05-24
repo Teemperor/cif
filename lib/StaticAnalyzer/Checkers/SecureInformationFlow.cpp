057e81a3845866b7f499c30518008e52685ff21f
Raphael Isemann <teemperor@gmail.com>
2018-05-24 10:41:34 +0200

    NFC refactoring and documentation

--- lib/StaticAnalyzer/Checkers/SecureInformationFlow.cpp
+++ lib/StaticAnalyzer/Checkers/SecureInformationFlow.cpp
@@ -30,16 +30,23 @@ using namespace ento;
 
 namespace {
 
+/// A security class, consisting of a set of labels.
 class SecurityClass {
+  /// The set of owners of
   std::set<std::string> Owners;
 public:
-  SecurityClass() {
-  }
+  SecurityClass() = default;
 
+  /// Parses the given string into a security class. The format is a
+  /// comma-separated list of owners.
+  /// Example input is "A,B" which would create a security class with the owners
+  /// A and B.
   static SecurityClass parse(StringRef S) {
     SecurityClass Result;
+    // Empty string means an empty security class.
     if (S.empty())
       return Result;
+    // Split the string into its owners and add each owner to the result.
     llvm::SmallVector<StringRef, 4> OwnerStrings;
     StringRef(S).split(OwnerStrings, ',');
     for (StringRef OwnerString : OwnerStrings) {
@@ -48,21 +55,34 @@ public:
     return Result;
   }
 
+  /// Parses the given CIFLabel in the format:
+  ///    InfoFlow|OWNERS
+  /// where OWNERS is a list of owners as described in SecurityClass::parse.
   static SecurityClass parseLabel(StringRef S) {
     auto Parts = S.split('|');
+    // If the prefix is our InfoFlow label, then parse the owners.
     if (Parts.first == "InfoFlow") {
       return parse(Parts.second);
     }
+    // Otherwise just return an empty security class as this was not a
+    // Cif label.
     return SecurityClass();
   }
 
+  /// Merges this security class with the given security class. The new security
+  /// class will be the least-restrictive security class that can hold all
+  /// information from both this label and the other.
   void mergeWith(const SecurityClass &Other) {
     for (auto &O : Other.Owners) {
       Owners.insert(O);
     }
   }
 
+  /// Returs true if it is allowed by the Cif information flow security that
+  /// data with the given label can flow to this label.
   bool allowsFlowFrom(const SecurityClass &Other) {
+    // The owners of the given security class needs to be a subset of the
+    // current class that the flow is allowed.
     for (auto &O : Other.Owners) {
       if (Owners.find(O) == Owners.end())
         return false;
@@ -70,64 +90,93 @@ public:
     return true;
   }
 
+  /// Returns a human-readable string representation of this security class.
   std::string getLabel() const {
     if (Owners.empty())
       return "<NO-LABEL>";
     return llvm::join(Owners, ",");
   }
 
+  /// Returns true if this security class is not the "bottom" security class
+  /// representing unclassified data.
   operator bool() const {
     return !Owners.empty();
   }
 
+  /// Dumps the information about this security class to the LLVM error stream.
+  /// For debugging purposes.
   void dump() const {
     llvm::errs() << "SecurityClass: " << getLabel() << "\n";
   }
 };
 
+/// The security context in which a statement can evaluated.
+///
+/// The context contains additional security classes for any data evaluated in
+/// the current state of the program. A context can get a more restrictive
+/// security class by implicit flows that depend on classified information.
 class SecurityContext {
   std::vector<std::pair<SecurityClass, Stmt *> > Context;
   SecurityClass ContextClass;
 public:
   SecurityContext() = default;
+  /// Adds the given security class to the context. This will increase the
+  /// security class of the context to the least-restrictive class that
+  /// can hold information from the new class and the existing context class.
   void add(SecurityClass NewClass, Stmt *Cause) {
     Context.emplace_back(NewClass, Cause);
     ContextClass.mergeWith(NewClass);
   }
 
+  /// Returns the security class of the context that should be additionally
+  /// applied to every information evaluated in this context.
   SecurityClass getClass() const {
     return ContextClass;
   }
 };
 
+/// Implements the Cif secure information flow policy.
 class SecureInformationFlow
     : public Checker<check::EndOfTranslationUnit> {
   mutable std::unique_ptr<BugType> BT_Exact;
 
+  /// A violation of the policy that was found in the program while verifying.
   struct Violation {
+    /// The statement that is violating the policy.
     Stmt *ViolatingStmt;
+    /// The source of the information that violated the information flow. May
+    /// be null in case the source of the information is not clear.
     Stmt *Source;
     SecurityClass TargetClass, SourceClass;
     SourceRange TargetLoc, SourceLoc;
   };
+  /// A list of all found violations by the verifier.
   std::vector<Violation> Violations;
 
+  /// A map of decls to a flag that designed whether they are pure or not.
   std::unordered_map<const Decl *, bool> PureDecls;
 
+  /// Marks the given decl as pure (or as not pure).
   void markAsPure(const Decl *D, bool IsPure = true) {
+    // No decl given means no work to do.
     if (!D)
       return;
+    // For function template decls we also mark all specializations as pure.
     if (const FunctionTemplateDecl *TFD = dyn_cast<const FunctionTemplateDecl>(D)) {
       for (const auto &Spez : TFD->specializations())
         markAsPure(Spez);
     }
+    // Same for class templates.
     if (const ClassTemplateDecl *TD = dyn_cast<const ClassTemplateDecl>(D)) {
       for (const auto &Spez : TD->specializations())
         markAsPure(Spez);
     }
+    // Mark the canonical decl as pure.
     PureDecls[D->getCanonicalDecl()] = IsPure;
   }
 
+  /// Returns if the given decl has a attribute designating as 'pure' in the
+  /// Cif information policy model. Doesn't check redecls.
   bool isPureByAttrImpl(const Decl *D) {
     bool Result = false;
     auto Attrs = D->specific_attrs<AnnotateAttr>();
@@ -137,8 +186,10 @@ class SecureInformationFlow
     }
     return Result;
   }
-
+  /// Returns if the given decl has a attribute designating as 'pure' in the
+  /// Cif information policy model.
   bool isPureByAttr(const Decl *D) {
+    // Walk over all redecls. For now we only support functions.
     if (const FunctionDecl *FD = dyn_cast<const FunctionDecl>(D)) {
       for (const auto &Redecl : D->redecls()) {
         if (isPureByAttrImpl(Redecl))
@@ -148,18 +199,24 @@ class SecureInformationFlow
     return isPureByAttrImpl(D);
   }
 
+  /// Return true if the given decl is pure.
   bool isPure(const Decl *D) {
     if (D == nullptr)
       return false;
+    // We look up the canonical decl in the PureDecls map. If we found a result,
+    // we return it.
     auto CD = D->getCanonicalDecl();
     auto It = PureDecls.find(CD);
     if (It != PureDecls.end()) {
       return It->second;
     }
+    // Otherwise we manually look up this decl and cache the result in the
+    // PureDecl.
     bool Result;
     markAsPure(CD, Result = isPureByAttr(CD));
     return Result;
   }
+
 
   bool assertAccess(const SecurityContext &Ctxt, Stmt *ViolatingStmt,
                     SecurityClass TargetClass, SourceRange TargetRange,
@@ -206,10 +263,13 @@ class SecureInformationFlow
                         Source, ViolatingStmt);
   }
 
+  /// Iterates over each redecl of the given ParmVarDecl.
   void foreachParamRedecl(const ParmVarDecl *D, std::function<void(const ParmVarDecl*)> Func) {
     auto C = D->getDeclContext();
     const FunctionDecl *const FD = dyn_cast_or_null<const FunctionDecl>(C);
+    // First check if the param was declared in a function.
     if (FD) {
+      // Find the index of the given parameter in its parent function.
       unsigned ParamIndex = 0;
       bool FoundParam = false;
       for(; ParamIndex < FD->getNumParams(); ++ParamIndex) {
@@ -219,6 +279,8 @@ class SecureInformationFlow
           break;
         }
       }
+      // If we found a valid parameter index, we iterate over all redecls and
+      // call our function on it.
       if (FoundParam) {
         for (const FunctionDecl *Redecl : FD->redecls()) {
           if (ParamIndex < Redecl->getNumParams()) {
@@ -230,13 +292,18 @@ class SecureInformationFlow
     }
   }
 
+  /// Whether this parameter is a 'out' parameter in the Cif information flow
+  /// policy.
   bool isOutParam(const ParmVarDecl *D) {
     if (D == nullptr)
       return false;
 
     bool Result = false;
+    // Iterate over each redecl of this parameter to search for the attribute.
     foreachParamRedecl(D, [&Result](const ParmVarDecl *Other) {
       auto Attrs = Other->specific_attrs<AnnotateAttr>();
+      // If any attribute has the "InfoFlow-Out" text, we know that the user
+      // has marked this as a pure out parameter.
       for (const auto &A : Attrs) {
         StringRef AS = A->getAnnotation();
         Result |= (AS == "InfoFlow-Out");
@@ -245,6 +312,7 @@ class SecureInformationFlow
     return Result;
   }
 
+  /// Parses the attributes of the given decl for a security class.
   SecurityClass getSecurityClassAttrs(const Decl *D) {
     SecurityClass Result;
     auto Attrs = D->specific_attrs<AnnotateAttr>();
@@ -255,12 +323,22 @@ class SecureInformationFlow
     return Result;
   }
 
+  /// Returns the security class of the given decl.
+  /// The CheckRedecls parameter is used when the caller does not want to visit
+  /// all the redecls to determine the security class (for example because the
+  /// caller is already iterating over the redecls and we don't want infinite
+  /// recursion).
   SecurityClass getSecurityClass(const Decl *D, bool CheckRedecls = true) {
     if (D == nullptr)
       return SecurityClass();
 
+    // First get the security class of the decl itself by reading its
+    // attributes.
     SecurityClass Result = getSecurityClassAttrs(D);
 
+    // Now we handle all the special cases.
+
+    // For parameter decls we have to check all redecls.
     if (const ParmVarDecl *PD = dyn_cast<const ParmVarDecl>(D)) {
       if (CheckRedecls) {
         foreachParamRedecl(PD, [&Result, this](const ParmVarDecl *OtherP){
@@ -268,28 +346,39 @@ class SecureInformationFlow
         });
       }
     }
+    // For method decls, we merge the security class of the class with the
+    // security class of the return value.
     if (const CXXMethodDecl *MD = dyn_cast<const CXXMethodDecl>(D)) {
       Result.mergeWith(getSecurityClass(MD->getParent()));
     }
+    // The security class of a field decl also contains the security class of
+    // the class.
     if (const FieldDecl *FD = dyn_cast<const FieldDecl>(D)) {
       Result.mergeWith(getSecurityClass(FD->getParent()));
     }
+
     if (const CXXRecordDecl *RD = dyn_cast<const CXXRecordDecl>(D)) {
+      // Check the security classes of all redecls.
       if (CheckRedecls) {
         for(const auto &Redecl : RD->redecls()) {
           Result.mergeWith(getSecurityClass(Redecl, /*CheckRedecls*/false));
         }
       }
+      // Check the security classes of the base classes.
       for (auto &Base : RD->bases()) {
         TypeSourceInfo *T = Base.getTypeSourceInfo();
         const CXXRecordDecl *BaseDecl = T->getType().getTypePtrOrNull()->getAsCXXRecordDecl();
         Result.mergeWith(getSecurityClass(BaseDecl));
       }
     }
+    // Variable decls inherit he security classes of the used record and for
+    // pointers the pointee record.
     if (const VarDecl *VD = dyn_cast<VarDecl>(D)) {
       Result.mergeWith(getSecurityClass(VD->getType()->getAsCXXRecordDecl()));
       Result.mergeWith(getSecurityClass(VD->getType()->getPointeeCXXRecordDecl()));
     }
+    // Generic redecl check code.
+    // FIXME: Is this redundant with above's redecl walking code?
     if (CheckRedecls) {
       const Decl *Redecl = D->getMostRecentDecl();
       do {
@@ -303,6 +392,7 @@ class SecureInformationFlow
     return Result;
   }
 
+  // Gets the security class of the given stmt.
   SecurityClass getSecurityClass(Stmt *S) {
     if (S == nullptr)
       return SecurityClass();
@@ -314,7 +404,7 @@ class SecureInformationFlow
         BinaryOperator *BO = dyn_cast<BinaryOperator>(S);
         DeclassifyInfo D = tryAsDeclassify(BO);
         if (D.valid()) {
-          return D.getToClass();
+          return D.getTargetClass();
         }
         break;
       }
@@ -356,9 +446,11 @@ class SecureInformationFlow
     return Result;
   }
 
+  // Information about a declassify call. A declassify call moves information
+  // from one security class to another.
   class DeclassifyInfo {
     SecurityClass From;
-    SecurityClass To;
+    SecurityClass Target;
     Stmt *S = nullptr;
     Stmt *Child = nullptr;
     bool Valid = false;
@@ -376,7 +468,7 @@ class SecureInformationFlow
         abort();
       } else {
         Result.From = SecurityClass::parse(Parts.first);
-        Result.To = SecurityClass::parse(Parts.second);
+        Result.Target = SecurityClass::parse(Parts.second);
 
         Result.Valid = true;
       }
@@ -399,14 +491,17 @@ class SecureInformationFlow
     SecurityClass getFromClass() const {
       return From;
     }
-    SecurityClass getToClass() const {
-      return To;
+    SecurityClass getTargetClass() const {
+      return Target;
     }
 
+    // The source statement that is the input data with the expected 'From'
+    // security class.
     Stmt *getChild() const {
       return Child;
     }
 
+    // Returns the declassify statement itself.
     Stmt *getStmt() const {
       return S;
     }
